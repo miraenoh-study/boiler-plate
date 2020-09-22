52a94d0538703762a056d5559c817fef7a005d6b
miraenoh <yes_future@naver.com>
2020-09-22 12:24:37 +0900

    Added register endpoint

--- index.js
+++ index.js
@@ -3,20 +3,23 @@ const app = express()
 const port = 5000
 
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
 const config = require('./config/dev')
+const { User } = require('./models/User')
+
+// Configure bodyParser
+app.use(bodyParser.urlencoded({ extended: true })) // application/x-www-form-urlencoded
+app.use(bodyParser.json()) // application/json
 
 // Connect to mongodb
 mongoose
-  .connect(
-    config.mongodbConnextionString,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    }
-  )
+  .connect(config.mongodbConnextionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
   .then(() => {
     console.log('MongoDB Connected')
   })
@@ -26,6 +29,20 @@ mongoose
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+// Registration
+app.post('/register', (req, res) => {
+  // Get info for registration from the client
+  const user = new User(req.body)
+
+  // Save info into mongodb
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err })
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
